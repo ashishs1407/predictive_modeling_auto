a138fec533d33b3ae6c4b92e44298b7ec8917cd6
ashishs1407 <shimpi.ashish.me2@gmail.com>
2023-01-26 00:36:57 +0530

    update repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "pyc_regressor",
+    version= '0.0.1',
+    description="This is a pycaret regressor package.",
+    author="Ashish Shimpi",
+    author_email="a.shimpi93@gmail.com",
+    packages = find_packages(),
+    py_modules=[]
+
+)
