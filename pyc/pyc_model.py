16fcc2b5fa68562e0f1a50016ecd077008621912
ashishs1407 <shimpi.ashish.me2@gmail.com>
2023-01-26 05:53:36 +0530

    new code

--- pyc/pyc_model.py
+++ pyc/pyc_model.py
@@ -1,37 +1,38 @@
 import pycaret
 import pandas as pd
+import numpy as np
 from pycaret.regression import *
-
+from pycaret.utils import check_metric
 
 #check the shape of data
 
 class ModelTraining:
     def __init__(self) -> None:
         pass
-    def modeltraining(data_df,target_col):
+    def train(data_df,test_df,target_col):
     
-        #s = setup(df, target = 'SalePrice', session_id = 123, log_experiment = True, experiment_name = 'exp1')
         s = setup(data=data_df, target = target_col)
         #compare models
         best_model = compare_models()
-        with open ('best_model.txt', 'w') as file:  
-            file.write(str(best_model))  
+        predict_model(best_model)
+        save_model(best_model, 'my_model')
+        loaded_model = load_model('my_model')
 
-        return compare_models()
+        y_pred = predict_model(best_model,data=test_df)
+        r2 = check_metric(test_df[target_col], y_pred.Label, 'R2')
+        print(" the R2 score of model is : ", r2)
+
+        return y_pred ,loaded_model
+
+    def loadmodel(path):
+        return load_model(path)
+
+    def predict(test_df,best_model):
+        y_pred = predict_model(best_model,data=test_df)
+        
+        return y_pred
 
 
 
-
-#best_model = compare_models()
-
-#print(best_model)
-
-# compare models
-#best_model = compare_models()
-#with open ('all_models_compare.txt', 'w') as file:  
-    #file.write(str(best_model))  
-
-# save model
-#save_model(best_model, 'my_first_pipeline')
 
 
