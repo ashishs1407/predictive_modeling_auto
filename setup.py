cff0ea7b5215218d833efd8be300b64ee2bb472e
ashishs1407 <shimpi.ashish.me2@gmail.com>
2025-01-17 19:43:34 +0530

    updating readme.md

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
 from setuptools import setup,find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
 setup(
     name = "pyc_regressor",
     version= '0.0.2',
     description="This is a pycaret regressor package.",
-    long_description_content_type="text/markdown",
+    long_description=long_description
     LONG_DESCRIPTION = '''
                         [github repo - ](https://github.com/ashishs1407/predictive_modeling_auto)
                         ''',
     author="Ashish Shimpi",
     author_email="a.shimpi93@gmail.com",
     packages = find_packages(),
+    url='https://github.com/ashishs1407/predictive_modeling_auto'
     py_modules=[],
     keywords=['python', 'tutorial', 'Auto-regressor', 'ashish shimpi'],
     project_urls={
@@ -25,6 +30,7 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
                     
-    ]
+    ],
+    python_requires='>=3.6',
 
 )
